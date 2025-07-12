4e1e18fb802820f79ac8e58a157c2db3847b833c
Xasbus <davidbilliot0828.gmail.com>
2025-07-12 16:14:48 -0500

    adding picture switching branch

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar } from "./NavBar";
 import "./Home.scss";
+import NBSignature2 from "../../assets/NBSignaturePicture.jpg";
 
 export function Home() {
   return (
@@ -11,6 +12,13 @@ export function Home() {
       <main>
         <h1>Nicole B. Fletes</h1>
         <h2>test h2</h2>
+        <img
+          title="NBSignature"
+          alt="NBSignature"
+          width="20vw"
+          height="20vw"
+          src={NBSignature2}
+        />
       </main>
       <footer></footer>
     </>
