6f51cfc67752ad5f035b5b81ce26df7a14877d76
Xasbus <davidbilliot0828.gmail.com>
2026-02-20 20:01:01 -0600

    top section complete, adjusted sizes of signatures/image, transparent navbar

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.scss";
 import NBSignature1 from "../../assets/NoBGImage.png";
+import testNicPic from "../../assets/testNicPic.jpg";
 
 export function Home() {
   return (
@@ -15,8 +16,36 @@ export function Home() {
         </div>
 
         <div className="content-wrapper">
-          <h1>Home Page</h1>
+          <h3>Stories that bloom from imagination</h3>
         </div>
+        <br />
+        <section className="about-section">
+          <h2 className="about-title">About Nicole</h2>
+
+          <div className="about-container">
+            <div className="about-image-wrapper">
+              <img
+                src={testNicPic}
+                alt="Nicole B. Fletes portrait"
+                className="about-image"
+              />
+            </div>
+
+            <div className="about-text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+                nisi arcu. Aliquam tincidunt nisl non sapien sagittis, et
+                vestibulum justo vestibulum. Nunc pretium dapibus malesuada.
+                Fusce ac justo vulputate, vehicula orci laoreet, aliquam risus.
+                Maecenas eu ligula sed justo semper faucibus at a mi. Praesent
+                ante nisl, feugiat sit amet lobortis non, rhoncus ac nunc.
+                Quisque tincidunt, orci nec pellentesque rutrum, velit magna
+                luctus magna, id posuere magna nulla sit amet mauris. Ut
+                placerat ex ac metus luctus placerat.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
       <footer></footer>
     </>
